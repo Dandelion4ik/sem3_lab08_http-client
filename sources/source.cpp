91f9f5fec019e450827e411e418258c21e115c46
Dandelion4ik <Kaviandr@yandex.ru>
2021-01-26 09:51:26 +0300

    end_02

--- sources/source.cpp
+++ sources/source.cpp
@@ -3,7 +3,7 @@
 #include <client.hpp>
 
 int main(int argc, char* argv[]) {
-  Client client(argc, argv);
+  client client(argc, argv);
   return client.Run();
 }
 //./cmake-build-debug/tests  http://localhost:8080/v1/api/suggest
